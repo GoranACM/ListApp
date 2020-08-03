d364ca617e3442187731ce97289d0fae5f60d161
GoranACM <goranacm@gmail.com>
2020-08-03 21:39:54 +1000

    Add new items (small issue)

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, FlatList } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, FlatList, TextInput, Button } from 'react-native';
 import { Item } from './components/item';
 
 export default class App extends Component {
+
+  state = {
+    expenseAmount: 0,
+    expenseCategory: ''
+  }
 
   listData = [
     { id: '1', amount: 50, category: 'Food' },
@@ -14,10 +19,28 @@ export default class App extends Component {
   render() {
     return (
       <SafeAreaView style={styles.main}>
+      <Text>Add your expense</Text>
+      <TextInput style={ styles.input }
+        placeholder= "$ amount" 
+        placeholderTextColor= 'red'
+        onChangeText={
+          text => this.setState({ expenseAmount: parseFloat(text) })
+        }
+        keyboardType="number-pad"
+      />      
+      <TextInput style={ styles.input }
+        placeholder= "category" 
+        placeholderTextColor= 'red'
+        onChangeText={
+          text => this.setState({ expenseCategory: text })
+        }
+      />
+      <Button title="ADD" onPress={ () => addItem() }/>
         <FlatList
           data={ this.listData }
           renderItem={ this.renderList }
           keyExtractor={ item => item.id }
+          extraData={ this.state.expenseAmount }
         />
       </SafeAreaView>
     )
@@ -28,8 +51,30 @@ export default class App extends Component {
   )
 }
 
+addItem = () => {
+  if (isNaN(this.state.expenseAmount) || this.state.expenseAmount == 0) 
+  {
+    return;
+  }
+  let itemId = new Date().getTime().toString()
+  let item = {
+    id: itemId,
+    amount: this.state.expenseAmount,
+    category: this.state.expenseCategory
+  }
+  this.listData.push(item)
+  this.setState({expenseAmount: 0})
+}
+
 const styles = StyleSheet.create({
   main: {
     paddingHorizontal: 10,
+  },
+  input: {
+    width: '100%',
+    padding: 10,
+    borderColor: 'black',
+    borderWidth: 1,
+    marginVertical: 15,
   }
 })
