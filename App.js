cb2a5a79f2e7fa863e1b6ebba2e3ba704213bdb4
GoranACM <goranacm@gmail.com>
2020-08-03 19:01:29 +1000

    Render a list of items from an array

--- App.js
+++ App.js
@@ -1,19 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, FlatList } from 'react-native';
+import { Item } from './components/item';
 
 export default class App extends Component {
+
+  listData = [
+    { id: '1', amount: 50, category: 'Food' },
+    { id: '2', amount: 150, category: 'Groceries' },
+    { id: '3', amount: 30, category: 'Transport' },
+  ]
+
   render() {
     return (
-      <SafeAreaView>
-
+      <SafeAreaView style={styles.main}>
+        <FlatList
+          data={ this.listData }
+          renderItem={ this.renderList }
+          keyExtractor={ item => item.id }
+        />
       </SafeAreaView>
     )
   }
+
+  renderList = ({ item }) => (
+    <Item amount={ item.amount } category={ item.category }/>
+  )
 }
 
 const styles = StyleSheet.create({
   main: {
     paddingHorizontal: 10,
-  },
+  }
 })
