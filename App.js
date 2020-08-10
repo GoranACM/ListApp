2432a7afa7359a3fe48af43df845e1a0350ff0c6
GoranACM <goranacm@gmail.com>
2020-08-10 18:40:30 +1000

    Picker for categories

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, FlatList, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, FlatList, TextInput, Button, TouchableOpacity } from 'react-native';
 import { Item } from './components/item';
+// third-party components
+import RNPickerSelect from 'react-native-picker-select';
+import Picker from 'react-native-picker-select';
+// custom components
 
 export default class App extends Component {
 
@@ -10,38 +14,55 @@ export default class App extends Component {
     expenseCategory: ''
   }
 
-  listData = [
-    { id: '1', amount: 50, category: 'Food' },
-    { id: '2', amount: 150, category: 'Groceries' },
-    { id: '3', amount: 30, category: 'Transport' },
+  listData = []
+
+  dropdownItems = [
+    { label: 'Food', value: 'food' },
+    { label: 'Transport', value: 'transport' },
+    { label: 'Rent', value: 'rent' },
+    { label: 'Grocery', value: 'grocery' },
+    { label: 'Entertainment', value: 'entertainment' }
   ]
 
   render() {
     return (
-      <SafeAreaView style={styles.main}>
-      <Text>Add your expense</Text>
-      <TextInput style={ styles.input }
-        placeholder= "$ amount" 
-        placeholderTextColor= 'red'
-        onChangeText={
-          text => this.setState({ expenseAmount: parseFloat(text) })
-        }
-        keyboardType="number-pad"
-      />      
-      <TextInput style={ styles.input }
-        placeholder= "category" 
-        placeholderTextColor= 'red'
-        onChangeText={
-          text => this.setState({ expenseCategory: text })
-        }
-      />
-      <Button title="ADD" onPress={ () => addItem() }/>
-        <FlatList
-          data={ this.listData }
-          renderItem={ this.renderList }
-          keyExtractor={ item => item.id }
-          extraData={ this.state.expenseAmount }
-        />
+      <SafeAreaView>
+      <View style={styles.main}>
+        <Text>Add your expense</Text>
+          <TextInput style={ styles.input }
+            placeholder= "$ amount" 
+            placeholderTextColor= 'red'
+            onChangeText={
+              text => this.setState({ expenseAmount: parseFloat(text) })
+            }
+            keyboardType="number-pad"
+          />      
+          {/* <TextInput style={ styles.input }
+            placeholder= "category" 
+            placeholderTextColor= 'red'
+            onChangeText={
+              text => this.setState({ expenseCategory: text })
+            }
+          /> */}
+          <RNPickerSelect
+            items = { this.dropdownItems }
+            value = { this.state.expenseCategory }
+            onValueChange = { value => this.setState({ expenseCategory: value }) }
+            useNativeAndroidPickerStyle = { false }
+           />
+      </View>
+      <View>
+        <TouchableOpacity style={styles.button} onPress={ this.addItem }>
+          <Text style={styles.buttonText} >Add</Text>
+        </TouchableOpacity>
+      </View>
+        
+      <FlatList
+            data={ this.listData }
+            renderItem={ this.renderList }
+            keyExtractor={ item => item.id }
+            extraData={ this.state.expenseAmount }
+          />
       </SafeAreaView>
     )
   }
@@ -52,19 +73,22 @@ export default class App extends Component {
 }
 
 addItem = () => {
-  if (isNaN(this.state.expenseAmount) || this.state.expenseAmount == 0) 
-  {
+  if( 
+    isNaN(this.state.expenseAmount) || 
+    this.state.expenseAmount == 0 || 
+    this.state.expenseCategory == '' ) {
     return;
   }
   let itemId = new Date().getTime().toString()
-  let item = {
+  let listItem = {
     id: itemId,
     amount: this.state.expenseAmount,
     category: this.state.expenseCategory
   }
-  this.listData.push(item)
-  this.setState({expenseAmount: 0})
+  this.listData.push(listItem)
+  this.setState({expenseAmount:0})
 }
+
 
 const styles = StyleSheet.create({
   main: {
@@ -76,5 +100,13 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderWidth: 1,
     marginVertical: 15,
+  },
+  button: {
+    padding: 15,
+    backgroundColor: 'black'
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center'
   }
 })
