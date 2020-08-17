1a5e3a331cacd6743a762263b77b8a57c89dd7bd
GoranACM <goranacm@gmail.com>
2020-08-17 22:28:36 +1000

    Split up the styles and update expo

--- App.js
+++ App.js
@@ -1,81 +1,107 @@
-import { StatusBar } from 'expo-status-bar';
-import React , {Component} from 'react';
-import { StyleSheet, Text, View, SafeAreaView, FlatList, TextInput, TouchableOpacity, AsyncStorage } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React, { Component } from 'react'
+import {
+  Text,
+  View,
+  SafeAreaView,
+  FlatList,
+  TextInput,
+  TouchableOpacity,
+  AsyncStorage,
+} from 'react-native'
 // third-party components
-import RNPickerSelect from 'react-native-picker-select';
+import RNPickerSelect from 'react-native-picker-select'
 // custom components
-import {Item} from './components/item';
+import { Item } from './components/item'
+import { Bar } from './components/bar';
+// stylesheets
+import {styles} from './styles/main';
+import {pickerStyle} from './styles/picker';
+
+const pickerPlaceholder = {
+  label: 'select a category',
+  value: null,
+  color: 'black',
+}
 
 export default class App extends Component {
   state = {
-    expenseAmount : 0,
+    expenseAmount: 0,
     expenseCategory: '',
     validInput: false,
     showToast: false,
+    message: '',
   }
-  
   listData = []
 
   dropdownItems = [
-    { label: 'Food' , value: 'food' },
-    { label: 'Transport' , value: 'transport' },
-    { label: 'Rent' , value: 'rent' },
-    { label: 'Grocery' , value: 'grocery' },
-    { label: 'Entertainment' , value: 'entertainment' },
+    { label: 'Food', value: 'food' },
+    { label: 'Transport', value: 'transport' },
+    { label: 'Rent', value: 'rent' },
+    { label: 'Grocery', value: 'grocery' },
+    { label: 'Entertainment', value: 'entertainment' },
   ]
 
   render() {
     return (
-      <SafeAreaView style={{flex: 1, position: 'relative'}}>
+      <SafeAreaView style={{flex:1, position: 'relative'}}>
         <View style={styles.main}>
-        <Text>Add your expense</Text>
-        <TextInput 
-          style={styles.input}
-          placeholder="$ amount" 
-          onChangeText={ text => this.setState({expenseAmount: parseFloat(text) },
-            () => { this.validate() }
-          ) } 
-          keyboardType="number-pad" 
-          ref={(input) => ( this._textInput = input )}/>
-        {/* <TextInput
-          style={styles.input}
-          placeholder="category"
-          onChangeText={ text => this.setState({ expenseCategory: text }) }
-        /> */}
-          <RNPickerSelect 
-            items = { this.dropdownItems }
-            value = { this.state.expenseCategory }
-            onValueChange = { value => this.setState({expenseCategory: value},
-              () => { this.validate() }
-            ) }
-            useNativeAndroidPickerStyle = {false}
+        <Bar />
+          <View style={styles.bar}>
+            <Text>Add your expense</Text>
+          </View> 
+          <TextInput
+            style={styles.input}
+            placeholder="$ amount"
+            onChangeText={(text) =>
+              this.setState({ expenseAmount: parseFloat(text) }, () => {
+                this.validate()
+              })
+            }
+            keyboardType="number-pad"
+            ref={(input) => (this._textInput = input)}
+          />
+          <RNPickerSelect
+            items={this.dropdownItems}
+            value={this.state.expenseCategory}
+            onValueChange={(value) =>
+              this.setState({ expenseCategory: value }, () => {
+                this.validate()
+              })
+            }
+            useNativeAndroidPickerStyle={false}
             style={pickerStyle}
             placeholder={pickerPlaceholder}
           />
         </View>
         {/* wrap the button in view */}
-        <View>
-          <TouchableOpacity 
-            style={ this.state.validInput ? styles.button : styles.buttonDisabled } 
-            onPress= {this.addItem}
-            disabled = { !this.state.validInput ? true : false}
+        <View style={styles.buttonView}>
+          <TouchableOpacity
+            style={
+              [
+                this.state.validInput ? styles.button : styles.buttonDisabled,
+                {borderRadius: 10}
+              ]
+            }
+            onPress={this.addItem}
+            disabled={!this.state.validInput ? true : false}
           >
-            <Text style={styles.buttonText} >Add</Text>
+            <Text style={styles.buttonText}>Add</Text>
           </TouchableOpacity>
         </View>
         <View style={[{
           display: this.state.showToast ? 'flex' : 'none'
-        }, styles.toast]}>
-          <Text style={ styles.toastMessage }>Item Deleted!</Text>
+        }, styles.toast ]}>
+          <Text style={styles.toastMessage}>{this.state.message}</Text>
         </View>
-        <View style={{flex: 1}}>
-          <FlatList 
-            data = {this.listData}
-            renderItem = { this.renderList }
-            keyExtractor = { item => item.id }
-            extraData = {this.state.expenseAmount}
+        <View style={{flex:1}}>
+          <FlatList
+            data={this.listData}
+            renderItem={this.renderList}
+            keyExtractor={(item) => item.id}
+            extraData={this.state.expenseAmount}
           />
-        </View>       
+        </View>
       </SafeAreaView>
     )
   }
@@ -84,63 +110,64 @@ export default class App extends Component {
     this.loadList()
   }
 
-  renderList = ({item}) => (
+  renderList = ({ item }) => (
     <Item 
-      amount={ item.amount } 
-      category={ item.category } 
-      id={ item.id }
+      amount={item.amount} 
+      category={item.category} 
+      id={item.id} 
       delete={ this.removeItem }
     />
   )
 
   removeItem = (itemId) => {
-    this.listData.forEach( (item, index) => {
+    this.listData.forEach( (item,index) => {
       if (item.id == itemId) {
         this.listData.splice( index, 1 )
       }
-    })
-    this.showToast('Item Deleted!', 2000)
+    } )
+    this.showToast('Item deleted', 2000 )
     this.saveList()
-    this.setState({expenseAmount: 0})
+    this.setState({expenseAmount:0})
   }
 
   addItem = () => {
-    if( 
-      isNaN(this.state.expenseAmount) || 
-      this.state.expenseAmount == 0 || 
-      this.state.expenseCategory == '' ) {
-      return;
+    if (
+      isNaN(this.state.expenseAmount) ||
+      this.state.expenseAmount == 0 ||
+      this.state.expenseCategory == ''
+    ) {
+      return
     }
     let itemId = new Date().getTime().toString()
     let listItem = {
       id: itemId,
       amount: this.state.expenseAmount,
-      category: this.state.expenseCategory
+      category: this.state.expenseCategory,
     }
     this.listData.push(listItem)
-    // Sort list in descending order
+    // sort list in descending order
     this.sortList()
     this.saveList()
     this.setState({
-      expenseAmount:0, 
-      expenseCategory: null, 
-      validInput: false 
+      expenseAmount: 0,
+      expenseCategory: null,
+      validInput: false,
     })
     this._textInput.clear()
-    this._textInput.focus()
-    this.showToast('Item Added!', 1500)
+    //this._textInput.focus()
+    this.showToast('item added', 1500 )
   }
 
   validate = () => {
-    if( this.state.expenseAmount > 0 && this.state.expenseCategory ) {
-      this.setState({validInput:true})
+    if (this.state.expenseAmount > 0 && this.state.expenseCategory) {
+      this.setState({ validInput: true })
     }
   }
 
   sortList = () => {
-    this.listData.sort( (item1, item2) => {
+    this.listData.sort( (item1,item2) => {
       return item2.id - item1.id
-    })
+    } )
   }
 
   saveList = async () => {
@@ -149,93 +176,32 @@ export default class App extends Component {
         'data',
         JSON.stringify(this.listData)
       )
-    } catch (error) {
+    }
+    catch( error ) {
       console.log(error)
     }
   }
 
   loadList = async () => {
-    try {
+    try{
       let items = await AsyncStorage.getItem('data')
-      if (JSON.parse(items)) {
+      if( JSON.parse(items) ) {
         this.listData = JSON.parse( items )
       }
-      this.setState({expenseAmount: 0})
-    } catch (error) {
+      this.setState({expenseAmount:0})
+    }
+    catch(error) {
       console.log(error)
     }
   }
 
-  showToast = (message, duration) => {
-    this.setState({ 
-      message: message }, 
-      () => { this.setState({ showToast: true }) }
+  showToast = ( message, duration ) => {
+    this.setState({message: message }, 
+      () => { this.setState({showToast: true}) }
     )
     const timer = setTimeout( 
-      () => {this.setState({showToast: false})}, 
-      3000
+      () => { this.setState({showToast: false }) },
+      duration 
     )
   }
 }
-
-const colors = {
-  primary : 'hsla(330, 38%, 65%, 1)',
-  primaryDisabled: 'hsla(330, 38%, 80%, 1)',
-}
-
-const pickerPlaceholder = {
-  label: 'select category', value: null, color: 'black'
-}
-
-const styles = StyleSheet.create({
-  main: {
-    paddingHorizontal: 10,
-  },
-  input: {
-    width: '100%',
-    padding: 10,
-    borderColor: 'black',
-    borderWidth: 1,
-    marginVertical: 15
-  },
-  button: {
-    padding: 15,
-    backgroundColor: colors.primary,
-    marginVertical: 15
-  },
-  buttonText: {
-    color: 'white',
-    textAlign: 'center'
-  },
-  buttonDisabled: {
-    padding: 15,
-    backgroundColor: colors.primaryDisabled,
-    marginVertical: 15,
-  },
-  toast: {
-    position: 'absolute',
-    backgroundColor: 'black',
-    bottom: 20,
-    left: 20,
-    right: 20,
-    padding: 5,
-    borderRadius: 5,  
-  },
-  toastMessage: {
-    color: 'white',
-    textAlign: 'center',
-  }
-})
-
-const pickerStyle = StyleSheet.create({
-  inputIOS: {
-    padding: 10,
-    borderColor: colors.primary,
-    borderWidth: 1,
-  },
-  inputAndroid: {
-    padding: 10,
-    borderColor: colors.primary,
-    borderWidth: 1,
-  }
-})
--- components/item.js
+++ components/item.js
@@ -1,39 +1,18 @@
 import React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text,View,TouchableOpacity,Image } from 'react-native';
 
-export const Item = (props) => {
-    return(
-        <View style={ itemStyle.item }>
-            <View>
-                <Text style={ itemStyle.text }>{ props.category }</Text>
-                <Text style={ itemStyle.text }>{ props.amount }</Text>
-            </View>
-            <TouchableOpacity style={styles.icon} onPress={ () => { props.delete( props.id ) }}>
-                <Image source={require('../assets/trash-alt-solid.png')}></Image>
-            </TouchableOpacity>
-        </View>
-    )
+import {itemStyles} from '../styles/itemStyle';
+
+export const Item = ( props ) => {
+  return(
+    <View style={itemStyles.item}>
+      <View style={itemStyles.row}>
+        <Text style={itemStyles.text}>{props.category}</Text>
+        <Text style={itemStyles.text}>{props.amount}</Text>
+      </View>
+      <TouchableOpacity onPress={ () => {props.delete( props.id ) } }>
+        <Image style={itemStyles.icon} source={require('../assets/trash-alt-solid.png')} />
+      </TouchableOpacity>
+    </View>
+  )
 }
-
-const itemStyle = StyleSheet.create({
-    item: {
-        padding: 10,
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    text: {
-        fontSize: 16,
-        color: 'black',
-    },
-    row: {
-        flexDirection: 'row',
-        flex: 1,
-        justifyContent: 'space-between',
-        paddingRight: 10,
-    },
-    icon: {
-        width: 20,
-        height: 20,
-    }
-})
